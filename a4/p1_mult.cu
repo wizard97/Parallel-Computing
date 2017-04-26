b9a063d2c9e2ca9523704be90a0a987c1e673152
Aaron Wisner <daw268@cornell.edu>
2017-04-26 12:13:41 -0400

    Final submission

---
+++ a4/p1_mult.cu
@@ -0,0 +1,42 @@
+#include "matrix_multiply.h"
+
+__global__ void matrixMulCuda(uint32_t n, float *dev_A, float *dev_B, float *dev_C)
+{
+    float partial = 0.0;
+    uint32_t i = blockIdx.y * blockDim.y + threadIdx.y; // Row i of C
+    uint32_t j = blockIdx.x * blockDim.x + threadIdx.x; // Column j of C
+
+    for (uint32_t k = 0; k < n; k++)
+        partial += dev_A[n*i + k]*dev_B[n*k + j];
+
+    dev_C[n*i + j] = partial;
+}
+
+
+void matrixMul(uint32_t n, float *A, float *B, float *C, uint32_t block_dim)
+{
+    float *dev_A, *dev_B, *dev_C;
+    // copy A
+    cudaMalloc(&dev_A, sizeof(float[n][n]));
+    cudaMemcpy(dev_A, A, sizeof(float[n][n]), cudaMemcpyHostToDevice);
+
+    // Copy B
+    cudaMalloc(&dev_B, sizeof(float[n][n]));
+    cudaMemcpy(dev_B, (float*)B, sizeof(float[n][n]), cudaMemcpyHostToDevice);
+
+    // Allocate space for C
+    cudaMalloc(&dev_C, sizeof(float[n][n]));
+
+    dim3 Block(block_dim, block_dim);
+    dim3 Grid(n/Block.x, n/Block.y);
+
+    matrixMulCuda<<< Grid, Block>>>(n, dev_A, dev_B, dev_C);
+
+    cudaMemcpy(C, dev_C, sizeof(float[n][n]), cudaMemcpyDeviceToHost);
+
+    // Clean up memory
+    cudaFree(dev_A);
+    cudaFree(dev_B);
+    cudaFree(dev_C);
+
+}
